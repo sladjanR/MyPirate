3e2cec83d00a092100f629ad470f62a37d293a3f
Slađan Radovanović <sladjan.radovanovicc@gmail.com>
2024-05-21 18:59:37 +0200

    Movement of objects, collision with objects

--- code/level.py
+++ code/level.py
@@ -1,35 +1,56 @@
 from settings import *
-from sprites import Sprite
+from sprites import Sprite, MovingSprite
 from player import Player
 
 class Level:
     def __init__(self, tmx_map):
         self.display_surface = pygame.display.get_surface()
 
-        # Groups
+        # GROUPS
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()  # This group contains all collidable sprites
+        self.semi_collision_sprites = pygame.sprite.Group()
+
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
         # We want to target one specific layer
-
+        # tiles
         # x, y - is not pixel positions, it is positions in a grid, because of that we have * TILE_SIZE
         for x, y, surf in tmx_map.get_layer_by_name("Terrain").tiles():
             Sprite((x * TILE_SIZE,y * TILE_SIZE), surf, (self.all_sprites, self.collision_sprites))     # We have tuple of sprites
         # print(tmx_map)
 
+        # objects
         for obj in tmx_map.get_layer_by_name('Objects'):
             #print(obj)
             if obj.name == "player":
                 # With obj we gets original positions do not need to multiply with TILE_SIZE
-                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites) # THE PLAYER ONLY ACCESS TO collision_sprites it's not inside it
+                Player((obj.x, obj.y), self.all_sprites, self.collision_sprites, self.semi_collision_sprites) # THE PLAYER ONLY ACCESS TO collision_sprites it's not inside it (also for semi_collision_sprites)
                 # print(obj.x)
                 # print(obj.y)
+
+        # moving objects
+        for obj in tmx_map.get_layer_by_name("Moving Objects"):
+            # print(obj)
+            if obj.name == "helicopter":
+                if obj.width > obj.height: # Horizontal movement
+                    move_dir = "x"
+                    start_pos = (obj.x, obj.y + obj.height / 2)     # In tiled it is on the middle on top so we divide by 2
+                    end_pos = (obj.x + obj.width, obj.y + obj.height / 2)
+                
+                else: # Vertical
+                    move_dir = "y"
+                    start_pos = (obj.x + obj.width / 2, obj.y)
+                    end_pos = (obj.x + obj.width / 2, obj.y + obj.height)
+
+                speed = obj.properties["speed"]
+                MovingSprite((self.all_sprites, self.semi_collision_sprites), start_pos, end_pos, move_dir, speed)
+    
             
 
     def run(self, dt):
-        self.all_sprites.update(dt)
         self.display_surface.fill("black")
+        self.all_sprites.update(dt)
         self.all_sprites.draw(self.display_surface)
         
--- code/player.py
+++ code/player.py
@@ -1,7 +1,8 @@
 from settings import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, collision_sprites):
+    def __init__(self, pos, groups, collision_sprites, semi_collision_sprites):
         super().__init__(groups)
         self.image = pygame.Surface((48,56))
         self.image.fill("orange")
@@ -22,31 +23,43 @@ class Player(pygame.sprite.Sprite):
         #---------------------------
         # Collision
         self.collision_sprites = collision_sprites
+        self.semi_collision_sprites = semi_collision_sprites
         # print(self.collision_sprites) -> To see how big is our group of sprites
 
         # We want to know is player on surface, and if he is, then he can again jump
         self.on_surface = {"floor" : False, "left" : False, "right": False}
+        self.platform = None
 
+        # timer
+        self.timers= {
+            "wall jump" : Timer(350),
+            "wall slide block" : Timer(250),
+            "platform skip" : Timer(300)
+        }
 
     def input(self):
         keys = pygame.key.get_pressed()
         input_vector = vector(0,0)
 
-        if keys[pygame.K_RIGHT]:
-            # print("Right")
-            input_vector.x += 1
-        if keys[pygame.K_LEFT]:
-            # print("Left")
-            input_vector.x -= 1
-        
-        # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
-        self.direction.x = input_vector.normalize().x if input_vector else input_vector.x  # normalize() -> We want to be sure that length is ALWAYS 1
-        # Above we are used thernal operator because we cant normalize if vector have no elements
-        # We added later to normalize only x (horizontal), but for y we want to be more than 1 (be faster)
+        if not self.timers["wall jump"].active:
+            if keys[pygame.K_RIGHT]:
+                # print("Right")
+                input_vector.x += 1
+            if keys[pygame.K_LEFT]:
+                # print("Left")
+                input_vector.x -= 1
+            if keys[pygame.K_DOWN]:
+                self.timers["platform skip"].activate()
+            
+            # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+            self.direction.x = input_vector.normalize().x if input_vector else input_vector.x  # normalize() -> We want to be sure that length is ALWAYS 1
+            # Above we are used thernal operator because we cant normalize if vector have no elements
+            # We added later to normalize only x (horizontal), but for y we want to be more than 1 (be faster)
 
         if keys[pygame.K_SPACE]:
             # self.direction.y = -100 -> This can be feature :)
             self.jump = True
+            
 
     def move(self, dt):
         # horizontal
@@ -58,26 +71,64 @@ class Player(pygame.sprite.Sprite):
         # vertical
         # If we fall longer it be faster, because of that we use Data Time (dt)
         # Also we want to fall be frame independent, if framerate changes not change behavior (because of that we divide by 2 and use two times)
-        self.direction.y += self.gravity / 2  * dt
-        self.rect.y += self.direction.y * dt
-        self.direction.y += self.gravity / 2  * dt
+
+
+        if not self.on_surface["floor"] and any((self.on_surface["left"], self.on_surface["right"])) and not self.timers["wall slide block"].active:
+            self.direction.y = 0
+            self.rect.y += self.gravity / 10 * dt
+
+        else:
+            self.direction.y += self.gravity / 2  * dt
+            self.rect.y += self.direction.y * dt
+            self.direction.y += self.gravity / 2  * dt
+
+
+        # TODO: (srpski): OVAJ self.collision mi pravi problem, treba da je postavljen posle self.jump upita, ali jedino ovako radi
         self.collision("vertical")
+        self.semi_collision()
 
         if self.jump:
             if self.on_surface["floor"]:
                 self.direction.y = -self.jump_heigt
-            
-            # Be sure that is out of inside if 
+                self.timers["wall slide block"].activate()
+                self.rect.bottom -= 1   # Player is not glued to the platform
+            elif any((self.on_surface["left"], self.on_surface["right"])) and not self.timers["wall slide block"].active:  # Once player is on wall, and we try to jump, we activate timer and not allow any left and right input
+                self.timers["wall jump"].activate()     # To block two inputs (left and right)
+                self.direction.y = -self.jump_heigt
+                self.direction.x = 1 if self.on_surface["left"] else -1
             self.jump = False
-            
+
+        
+    def platform_move(self, dt):
+        if self.platform:
+            self.rect.topleft += self.platform.direction * self.platform.speed * dt       # We want to our player moves with platform with same speed and direction
     
     def check_contact(self):
         # We want to create a cuple of small rectangles around our player 1.08
-        floor_rect = pygame.Rect((self.rect.bottomleft,),(self.rect.width, 2))
+        floor_rect = pygame.Rect(self.rect.bottomleft,(self.rect.width, 2))
+        # Wall collision left and right 1.13
+        right_rect = pygame.Rect(self.rect.topright + vector(0, self.rect.height / 4), (2, self.rect.height / 2))
+        left_rect = pygame.Rect(self.rect.topleft  + vector(-2, self.rect.height / 4), (2, self.rect.height / 2))
+
+        # To see if it works
+        # pygame.draw.rect(self.display_surface, "yellow", floor_rect)
+        # pygame.draw.rect(self.display_surface, "yellow", right_rect)
+        # pygame.draw.rect(self.display_surface, "yellow", left_rect)
+
         collide_rects = [sprite.rect for sprite in self.collision_sprites]
+        semi_collide_rect = [sprite.rect for sprite in self.semi_collision_sprites]
 
         # collisions
-        self.on_surface["floor"] = True if floor_rect.collidelist(collide_rects) >= 0 else False
+        self.on_surface["floor"] = True if floor_rect.collidelist(collide_rects) >= 0 or floor_rect.collidelist(semi_collide_rect) >= 0 and self.direction.y >=0 else False
+        self.on_surface["right"] = True if right_rect.collidelist(collide_rects) >= 0 else False
+        self.on_surface["left"]  = True if left_rect.collidelist(collide_rects)   >= 0 else False
+        # print(self.on_surface)
+
+        self.platform = None
+        sprites = self.collision_sprites.sprites() + self.semi_collision_sprites.sprites()
+        for sprite in [sprite for sprite in sprites if hasattr(sprite, 'moving')]:
+            if sprite.rect.colliderect(floor_rect):
+                self.platform = sprite
 
 
         #TODO:
@@ -87,29 +138,50 @@ class Player(pygame.sprite.Sprite):
 
     def collision(self, axis):
         for sprite in self.collision_sprites:
-            if sprite.rect.colliderect(self.rect):
+            if sprite.rect.colliderect(self.rect):      
                 if axis == "horizontal":
                     # Left collision -> but this isn't enought
-                    if self.rect.left <= sprite.rect.right and self.old_rect.left >= sprite.old_rect.right:
+                    if self.rect.left <= sprite.rect.right and int(self.old_rect.left) >= sprite.old_rect.right:     # All old_rects in the int() function
 
                         self.rect.left = sprite.rect.right
 
                     # Right collision
-                    if self.rect.right >= sprite.rect.left and self.old_rect.right <= sprite.old_rect.left:
+                    if self.rect.right >= sprite.rect.left and int(self.old_rect.right) <= sprite.old_rect.left:
                         self.rect.right = sprite.rect.left
 
                 else: # Vertical collision
-                    if self.rect.top <=  sprite.rect.bottom and self.old_rect.top >= sprite.old_rect.bottom:
+                    # Top
+                    if self.rect.top <=  sprite.rect.bottom and int(self.old_rect.top) >= sprite.old_rect.bottom:
                         self.rect.top = sprite.rect.bottom
-
-                    if self.rect.bottom >= sprite.rect.top and self.old_rect.bottom <= sprite.old_rect.top:
+                        if hasattr(sprite, "moving"):
+                            self.rect.top += 6  # Fix bug when moving platform goes down, and player jumps, then player stucking in the middle of platform
+                                                # You can TODO add timer to disable collison for the better feeling
+                    # Bottom
+                    if self.rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:
                         self.rect.bottom = sprite.rect.top
                     
                     self.direction.y = 0
 
+    def semi_collision(self):
+        if not self.timers["platform skip"].active:
+            for sprite in self.semi_collision_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.rect.bottom >= sprite.rect.top and int(self.old_rect.bottom) <= sprite.old_rect.top:     # Added later to be in int()
+                        self.rect.bottom = sprite.rect.top
+                        if self.direction.y > 0:
+                            self.direction.y = 0
+
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
+
     # Override
     def update(self, dt):
         self.old_rect = self.rect.copy()    # Our old position before collision, to now from which side is collision, where is player before
+        self.update_timers()
         self.input()
         self.move(dt)
+        self.platform_move(dt)
         self.check_contact()
+        # print(self.timers["wall jump"].active)
